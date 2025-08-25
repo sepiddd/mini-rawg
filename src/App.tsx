b8c336837fcb22930f58fc17d226ffaf3214ffff
sepiddd <sepide.khadempir@gmail.com>
2025-08-25 13:27:06 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Box width="100%" padding="4" color="white">
+      <h1>Mini RAWG</h1>
+    </Box>
+  );
+}
+
+export default App;
---
+++ src/components/ui/Provider.tsx
@@ -0,0 +1,27 @@
+import {
+  ChakraProvider,
+  createSystem,
+  defaultConfig,
+  defineConfig,
+} from "@chakra-ui/react";
+import type { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+const config = {
+  theme: {
+    tokens: {
+      colors: {
+        brand: {},
+      },
+    },
+  },
+};
+
+const system = createSystem(defaultConfig, config);
+
+export default function Provider({ children }: Props) {
+  return <ChakraProvider value={system}>{children}</ChakraProvider>;
+}
