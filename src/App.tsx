9bdf240dc70b507453cfd350433f5488bb7455ed
sepiddd <sepide.khadempir@gmail.com>
2025-08-25 14:44:20 +0100

    build a nva bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
@@ -8,8 +9,8 @@ function App() {
         lg: `"nav nav" "aside main"`,
       }}
     >
-      <GridItem area={"nav"} bg="skyblue">
-        nav
+      <GridItem area={"nav"}>
+        <NavBar />
       </GridItem>
       <Show when={window.innerWidth > 1200}>
         <GridItem bg={"pink"} area={"aside"}>
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,10 @@
+import { HStack, Image, Text } from "@chakra-ui/react";
+
+const NavBar = () => (
+  <HStack>
+    <Image width={100} src="/logo.webp" />
+    <Text>Nav bar</Text>
+  </HStack>
+);
+
+export default NavBar;
